1315506a90c96d5221ebff7af38546ad37272b95
Anatolii Chupryna <anat.fulldev@gmail.com>
2024-11-02 04:21:46 -0500

    test of interacting with HTML

---
+++ 24092801/Interact_with_HTML.py
@@ -0,0 +1,15 @@
+import mechanicalsoup
+browser = mechanicalsoup.Browser()
+url ="http://olympus.realpython.org/login"
+login_page=browser.get(url)
+login_html=login_page.soup
+
+form=login_html.select('form')[0]
+form.select("input")[0]["value"]="zeus"
+form.select("input")[1]["value"]="ThunderDude"
+
+profile_page=browser.submit(form, login_page.url)
+links=profile_page.soup.select("a")
+base_url = "http://olympus.realpython.org"
+for link in links:
+    print(base_url + link["href"])
